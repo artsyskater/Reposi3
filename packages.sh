be516051eb324dc2d48b5b8ca0179997430f3b44
prof3ssorice <prof3ssorice@gmail.com>
2018-07-20 11:22:35 -0400

    added themes

--- packages.sh
+++ packages.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+dpkg-deb -bZgzip projects/asGlyphs-Black debs
 dpkg-deb -bZgzip projects/asGlyphs-White debs
+dpkg-deb -bZgzip projects/asGlyphs-Red debs
 # dkpg-deb -bZgzip projects/<project name> <output folder>
