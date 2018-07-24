cf5d62c84042bb1d48cc92f5999f8a810ebe43ed
prof3ssorice <prof3ssorice@gmail.com>
2018-07-24 18:16:48 -0400

    Added Rainbow Update

--- packages.sh
+++ packages.sh
@@ -2,4 +2,10 @@
 dpkg-deb -bZgzip projects/asGlyphs-Black debs
 dpkg-deb -bZgzip projects/asGlyphs-White debs
 dpkg-deb -bZgzip projects/asGlyphs-Red debs
+dpkg-deb -bZgzip projects/asGlyphs-Blue debs
+dpkg-deb -bZgzip projects/asGlyphs-Green debs
+dpkg-deb -bZgzip projects/asGlyphs-Indigo debs
+dpkg-deb -bZgzip projects/asGlyphs-Orange debs
+dpkg-deb -bZgzip projects/asGlyphs-Violet debs
+dpkg-deb -bZgzip projects/asGlyphs-Yellow debs
 # dkpg-deb -bZgzip projects/<project name> <output folder>
